f9e4fd4cbd9740981ffb865234728e0c5fbcfbb3
UT <tewariutkarsh@outlook.com>
2022-06-22 18:01:32 +0530

    Attempt #1: Build and Push  on Docker Hub, Heroku and AWS

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS, cross_origin
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=['POST', 'GET'])
+@cross_origin()
+def index():
+
+    return "Just a Flask App, updates in progress...."
+
+
+port = os.getenv("PORT", 80)
+if __name__ == '__main__':
+
+    # host = '0.0.0.0'
+    app.run(debug=True, port=port)
